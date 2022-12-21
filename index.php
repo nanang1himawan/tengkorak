0ce34257455f0de5c7379bbe583471a1c2840cd3
nanang1himawan <44635365+nanang1himawan@users.noreply.github.com>
2022-12-22 00:53:19 +0700

    project

---
+++ index.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Tengkorak Beats</title>
+        <link rel="stylesheet" type="text/css" href="css-main.css" >
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+        <!-- NavBar-->
+        <header class="header">
+            <nav class="navbar navbar-expand-lg fixed-top py-1 navbar-dark">
+                  <a class="navbar ms-3" href="#">
+                    <img src="../ProjectWebNovri/images/logo_header.png" width="170" height="50" alt="">
+                  </a>
+                    <div class="collapse navbar-collapse">
+                        <ul class="navbar-nav ms-auto mb-lg-0">
+                            <li class="nav-item active"><a href="#" class="me-5 nav-link text-uppercase"> HOME</a></li>
+                            <li class="nav-item"><a href="#" class="me-5 nav-link text-uppercase ">ABOUT</a></li>
+                            <li class="nav-item"><a href="#" class="me-5 nav-link text-uppercase ">MUSIC WORKS</a></li>
+                            <li class="nav-item"><a href="#" class="me-3 nav-link text-uppercase ">CONTACT</a></li>
+                        </ul>
+                </div>
+            </nav>
+          </header>
+          <!--END NavBar-->
+
+          <!-- Heroes-->
+          <section id="banner" >
+            <div class="banner row align-items-end">
+                <div class="col-md-5 offset-md-1" style="margin-bottom: 8.3333333333%" >
+                    <div style="font-size: 48px; font-style: normal; font-weight: 700;"><span style="color: #ffffff">TENGKORAK</span> <span style="color: #8BC53F"> BEATS </span></div>
+                    <div style="font-size: 17px; color: #ffffff;" ><span> Beats Maker - Producer - Mixing & Mastering Engineer </span></div>
+                </div>
+                
+              </div>
+        </section>
+        <!-- END Heroes-->
+
+
+
+
+        <!-- About section-->
+        <section id="about">
+            <div class="about d-flex justify-content-center">
+                <div style="backdrop-filter: brightness(40%);">
+                <div class="konten">
+                        <div style=" text-align: center; font-size: 96px; font-style: normal; font-weight: 700;"><p>ABOUT</p> </div>
+                        <div style="font-size: 24px;" ><p> I don't why making music can isolate me for an hour, some people say it's called passion. I never stop doing that from the first place until now, and I plan it for the rest of my life. Leaving my education degree and choosing Audio Engineer Class to make me become not just a composer and beat maker. Even better, my daily work life is in music and podcast studios as a sound engineer.<br> 
+                            <br> Sometimes I become Voice Over Director—even myself a Voice Actor. What a journey that I've been through make me have so many references to full fill my creativity and imagination for any and many projects, talents/artists, and clients from a variety of genre and concepts. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- END About section-->
+
+        <!-- <section id="porto">
+        
+            <div class="container text-center my-3">
+                <h2 class="font-weight-light">Bootstrap Multi Slide Carousel</h2>
+                <div class="row mx-auto my-auto justify-content-center">
+                    <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner" role="listbox">
+                            <div class="carousel-item active">
+                                <div class="col-md-3">
+                                    <div class="card">
+                                        <div class="card-img">
+                                            <img src="//via.placeholder.com/500x400/31f?text=1" class="img-fluid">
+                                        </div>
+                                        <div class="card-img-overlay">Slide 1</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="col-md-3">
+                                    <div class="card">
+                                        <div class="card-img">
+                                            <img src="//via.placeholder.com/500x400/e66?text=2" class="img-fluid">
+                                        </div>
+                                        <div class="card-img-overlay">Slide 2</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="col-md-3">
+                                    <div class="card">
+                                        <div class="card-img">
+                                            <img src="//via.placeholder.com/500x400/7d2?text=3" class="img-fluid">
+                                        </div>
+                                        <div class="card-img-overlay">Slide 3</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="col-md-3">
+                                    <div class="card">
+                                        <div class="card-img">
+                                            <img src="//via.placeholder.com/500x400?text=4" class="img-fluid">
+                                        </div>
+                                        <div class="card-img-overlay">Slide 4</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="col-md-3">
+                                    <div class="card">
+                                        <div class="card-img">
+                                            <img src="//via.placeholder.com/500x400/aba?text=5" class="img-fluid">
+                                        </div>
+                                        <div class="card-img-overlay">Slide 5</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <div class="col-md-3">
+                                    <div class="card">
+                                        <div class="card-img">
+                                            <img src="//via.placeholder.com/500x400/fc0?text=6" class="img-fluid">
+                                        </div>
+                                        <div class="card-img-overlay">Slide 6</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        </a>
+                        <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        </a>
+                    </div>
+                </div>
+                <h5 class="mt-2 fw-light">advances one slide at a time</h5>
+            </div>
+        
+        </section> -->
+
+
+
+
+        
+
+
+
+        <!-- Footer-->
+        
+    </body>
+
+</html>
+
+<script>
+    let items = document.querySelectorAll('.carousel .carousel-item');
+
+    items.forEach((el) => {
+        const minPerSlide = 3
+        let next = el.nextElementSibling;
+        for (var i=1; i<minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0];
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling;
+        }
+    })
+
+</script>
