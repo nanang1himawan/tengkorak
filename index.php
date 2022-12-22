cdf7754d7c35457714c2867d8df7f12ee19817ff
nanang1himawan <44635365+nanang1himawan@users.noreply.github.com>
2022-12-23 01:14:05 +0700

    latihan Pooo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <title>Tengkorak Beats</title>
         <link rel="stylesheet" type="text/css" href="css-main.css" >
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        <!-- JavaScript Bundle with Popper -->
+        <!-- JavaScript Bundle with Poooooooooooooooooooooooooooooooooooooooooooopper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
     </head>
